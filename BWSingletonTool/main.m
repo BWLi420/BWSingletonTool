6765042cecb413cf391164be2cd4f52191627126
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-01 12:11:59 +0800

    初始化项目

---
+++ BWSingletonTool/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BWSingletonTool
+//
+//  Created by mortal on 16/10/1.
+//  Copyright © 2016年 mortal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
