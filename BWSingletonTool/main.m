32991d3e51549010795cf153d9ac8fb5c49e7e3d
Mortal_Master <mortal@mortaldemacbook-pro.local>
2016-10-01 12:19:37 +0800

    测试工具类可用

--- BWSingletonTool/main.m
+++ BWSingletonTool/main.m
@@ -7,11 +7,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BWSingletonTool.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        //测试单例数据
+        BWSingletonTool *p2 = [BWSingletonTool new];
+        BWSingletonTool *p1 = [[BWSingletonTool alloc] init];
+        BWSingletonTool *p3 = [BWSingletonTool shareTool];
         
+        //打印地址
+        NSLog(@"p1 = %p", p1);
+        NSLog(@"p2 = %p", p2);
+        NSLog(@"p3 = %p", p3);
     }
     return 0;
 }
